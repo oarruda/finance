72d2654896a062b40888bf5ac53777d6aaa5794c
Rafael Arruda <rafa.lucas.arruda@gmail.com>
2025-11-21 18:02:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8444859572-1c9a4",
+  "appId": "1:192413104190:web:50852c0e04ac05f8a7f96f",
+  "apiKey": "AIzaSyDinaZcQc5_Q6Fg8YBixq94-CRRBZKRAwM",
+  "authDomain": "studio-8444859572-1c9a4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "192413104190"
+};
