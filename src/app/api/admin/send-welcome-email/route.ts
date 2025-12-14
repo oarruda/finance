a93f69472c8d7fa809eb32e43a54c1bef46df40a
oarruda <rafael@rafaelarruda.com>
2025-12-14 22:28:58 +0000

    Commit#16

--- src/app/api/admin/send-welcome-email/route.ts
+++ src/app/api/admin/send-welcome-email/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
-import WelcomeEmail from '@/components/emails/welcome-email';
+import { WelcomeEmail } from '@/components/emails/welcome-email';
 import { firebaseConfig } from '@/firebase/config';
 
 export async function POST(request: NextRequest) {
