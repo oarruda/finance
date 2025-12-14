32f7068f6a0d1d89d779bdcbef377e3e0798f531
oarruda <rafael@rafaelarruda.com>
2025-12-14 17:28:53 +0000

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/finance',
+  assetPrefix: '/finance/',
   typescript: {
     ignoreBuildErrors: true,
   },
