64da81a9e9717c0914460c69cc5c453adf203f76
oarruda <rafael@rafaelarruda.com>
2025-12-14 17:12:58 +0000

    commit#09

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/finance' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/finance/' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
