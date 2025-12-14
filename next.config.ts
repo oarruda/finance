ef8a190ed613e0807e43df7c2a9b771261b37c02
oarruda <rafael@rafaelarruda.com>
2025-12-14 19:02:08 +0000

    Revert to server mode for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/finance',
-  assetPrefix: '/finance/',
   typescript: {
     ignoreBuildErrors: true,
   },
